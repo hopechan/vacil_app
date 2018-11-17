15bc5c05c1697288934fc1da6e1fdf31609b3b4f
ramos95 <eramos.9588@gmail.com>
2018-11-16 20:10:40 -0600

    El menu para la página principal

--- lib/vistas/home.dart
+++ lib/vistas/home.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import './opciones.dart';
+
 String dato;
 class Home extends StatefulWidget {
   HomeState  createState()=> HomeState();
@@ -28,6 +29,50 @@ class HomeState extends State<Home> {
               Icon(FontAwesomeIcons.taxi),
               Icon(FontAwesomeIcons.walking)
             ],
+          ),
+          drawer: new Drawer(
+            child: new ListView(
+              children: <Widget>[
+                new UserAccountsDrawerHeader(
+                  accountName: new Text("usuario"),
+                  accountEmail: new Text("usuario@user.com"),
+                  currentAccountPicture: new GestureDetector(
+                    onTap: () => print("Se ha loggeado como usuario"),
+                    child: CircleAvatar(
+                      backgroundImage: new NetworkImage("https://img.fireden.net/v/image/1487/68/1487681871114.jpg"),
+                    ),
+                  ),
+                  decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                      image: new NetworkImage("https://img00.deviantart.net/35f0/i/2015/018/2/6/low_poly_landscape__the_river_cut_by_bv_designs-d8eib00.jpg"),
+                      fit: BoxFit.fill
+                    )
+                  ),
+                ),
+                new ListTile(
+                  title: new Text("mis rutas"),
+                  trailing: new Icon(Icons.location_on),
+                ),
+                new ListTile(
+                  title: new Text("opciones"),
+                  trailing: new Icon(Icons.settings),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext contex) => new Opciones()));
+                  } 
+                ),
+                new ListTile(
+                  title: new Text("cerrar"),
+                  trailing: new Icon(Icons.close),
+                  onTap: () => Navigator.of(context).pop(),
+                ),
+                new Divider(),
+                new ListTile(
+                  title: new Text("Logout"),
+                  trailing: new Icon(Icons.arrow_back),
+                )
+              ],
+            )
           ),
         )
       )
---
+++ lib/vistas/opciones.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Opciones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(title: Text("Opciones")),
+      body: new Center(
+        child: new Text("Página de opnciones"),
+        
+      ),
+      
+    );
+  }
+}
